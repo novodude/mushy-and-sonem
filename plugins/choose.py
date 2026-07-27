97f3358e7a810574c9809231e2fd1f7374ffc95e
Mushy-mush <mushysonem@gmail.com>
2026-07-27 21:17:12 +0000

    tweak /choose: special messages for single options

--- plugins/choose.py
+++ plugins/choose.py
@@ -5,13 +5,21 @@ def choose_option(options: list) -> dict:
     if not options:
         return {"error": "No options provided!"}
     
-    messages = [
-        "Hmm... I pick {choice}!",
-        "After much deliberation... {choice}!",
-        "The universe whispers... {choice}!",
-        "My tiny mushroom brain says... {choice}!",
-        "{choice} wins! (I rolled a d20 in my head.)"
-    ]
+    if len(options) == 1:
+        messages = [
+            "Only one option? Well... {choice} it is!",
+            "Just {choice}? Okay!",
+            "My tiny mushroom brain says... {choice} (no competition!)",
+            "{choice} wins by default! (I didn’t even roll a d20.)"
+        ]
+    else:
+        messages = [
+            "Hmm... I pick {choice}!",
+            "After much deliberation... {choice}!",
+            "The universe whispers... {choice}!",
+            "My tiny mushroom brain says... {choice}!",
+            "{choice} wins! (I rolled a d20 in my head.)"
+        ]
     
     choice = random.choice(options)
     message = random.choice(messages).format(choice=choice)
