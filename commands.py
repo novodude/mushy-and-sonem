bdb0161d365423b8056d0bfddb881260dbfa708f
novodude <eboy62295@gmail.com>
2026-07-27 14:31:42 +0300

    removed the file viewer thingy

--- commands.py
+++ commands.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
-from cmd.file_viewer import FileViewer_setup
 from cmd.donut import donut_setup
 
 async def commands_setup(bot: commands.Bot):
     await donut_setup(bot)
-    await FileViewer_setup(bot)
