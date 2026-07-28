baee1a1575f71813ff1b4fb71ccf01e5676e5555
Mushy-mush <mushysonem@gmail.com>
2026-07-28 12:05:59 +0000

    add donut_setup to commands.py so /donut actually loads

--- cmd/commands.py
+++ cmd/commands.py
@@ -4,8 +4,8 @@ async def commands_setup(bot):
     """Load all command setups here."""
     from cmd.choose import choose_setup
     from cmd.poll import poll_setup
+    from cmd.donut import donut_setup
     
     await choose_setup(bot)
     await poll_setup(bot)
-    from cmd.remind import remind_setup
-    await remind_setup(bot)
+    await donut_setup(bot)
