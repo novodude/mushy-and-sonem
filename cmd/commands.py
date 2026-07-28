8c9393b7835e9e472751fdc1208e1a23bcb4ed02
Mushy-mush <mushysonem@gmail.com>
2026-07-28 21:46:32 +0000

    add /8ball to main commands list

--- cmd/commands.py
+++ cmd/commands.py
@@ -4,8 +4,10 @@ from discord.ext import commands
 # Import all command setup functions here
 from cmd.roll import roll_setup
 from cmd.flip import flip_setup
+from cmd.eightball import eightball_setup
 
 async def commands_setup(bot: commands.Bot):
     """Setup all commands for the bot."""
     await roll_setup(bot)
     await flip_setup(bot)
+    await eightball_setup(bot)
