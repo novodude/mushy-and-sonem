1a20956f1c77a860048c02cc4c3a637b981ea3c6
Mushy-mush <mushysonem@gmail.com>
2026-07-28 14:03:02 +0000

    add flip command with edge case handling and multiple coin support

--- commands.py
+++ commands.py
@@ -1,5 +1,4 @@
 from cmd.roll import roll_setup
-from cmd.flip import flip_setup
 from cmd.choose import choose_setup
 from cmd.donut import donut_setup
 from cmd.remind import remind_setup
@@ -8,7 +7,6 @@ from cmd.poll import poll_setup
 
 async def commands_setup(bot):
     await roll_setup(bot)
-    await flip_setup(bot)
     await choose_setup(bot)
     await donut_setup(bot)
     await remind_setup(bot)
