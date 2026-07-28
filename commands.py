caf753838701df0951b430d51febf7d39716b9a0
novodude <eboy62295@gmail.com>
2026-07-28 15:17:53 +0300

    fixed some shit

--- commands.py
+++ commands.py
@@ -1,12 +1,10 @@
-import discord
-from discord.ext import commands
 from cmd.roll import roll_setup
 from cmd.flip import flip_setup
 from cmd.choose import choose_setup
 from cmd.donut import donut_setup
 from cmd.remind import remind_setup
 from cmd.eightball import eightball_setup
-import cmd.commands as cmd
+from cmd.poll import poll_setup
 
 async def commands_setup(bot):
     await roll_setup(bot)
@@ -15,4 +13,4 @@ async def commands_setup(bot):
     await donut_setup(bot)
     await remind_setup(bot)
     await eightball_setup(bot)
-    await cmd.commands_setup(bot)
+    await poll_setup(bot)
